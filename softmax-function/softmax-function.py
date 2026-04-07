ca8701fc3326ac33f068b009d0c5fef6b0ead21b
Nesrine <163661643+NesrineTahmi@users.noreply.github.com>
2026-04-07 03:11:25 +0100

    Update solution for softmax-function

--- softmax-function/softmax-function.py
+++ softmax-function/softmax-function.py
@@ -15,9 +15,7 @@ def softmax(x):
         axis = 1
     
     max  = np.max(x, axis, keepdims=True)
-    print(f"max : {max}")
     exp = np.exp(x - max)
-    print(f"exp : {exp}")
 
     sum = np.sum(exp, axis, keepdims=True)
     
